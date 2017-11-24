27febe15eced1ebd291fbe1047839419f2ca8397
Trashgen_Genderovich Aka Plazmovich <a.yvarov@yandex.ru>
2017-11-24 19:38:16 +0700

    #

--- source/Util.cpp
+++ source/Util.cpp
@@ -1,5 +1,12 @@
 #include"Util.hpp"
 
+#define ERRORREAD(path){\
+	fprintf(stderr,"CANNOT READ %s!\n",path);\
+	free(result);\
+	fclose(img);\
+	return 0;\
+}
+
 namespace Util{
 
 void *
@@ -9,15 +16,18 @@ void * result;
 
 {
 FILE * img = fopen(path,"rb");
-if(!img){
-	exit(1);
-}
+if(!img)
+	exit(3);
+
 
 fseek(img,0,SEEK_END);
 size_t sfile = ftell(img);
 rewind(img);
 result = malloc(sfile);
-fread(result, sfile, 1, img);
+
+if(!result) exit(3);
+
+if(!fread(result, sfile, 1, img)) ERRORREAD(path);
 fclose(img);
 }
 
@@ -25,6 +35,8 @@ fclose(img);
 return result;
 
 }
+
+
 
 void 
 usseles::FillScreen(int color){
